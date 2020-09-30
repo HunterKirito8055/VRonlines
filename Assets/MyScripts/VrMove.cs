64fd1868cd107790cb80d9a801b01f4c06d761a0
Vishal Kumar Varma <sirikondavishal8055@gmail.com>
2020-09-30 13:10:11 +0530

    VR upto movement

---
+++ Assets/MyScripts/VrMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrMove : MonoBehaviour
+{
+    bool isClicked;
+    public float SpeedMove = 1.5f;
+    bool isPlayerRotate = true;
+    bool isrotate;
+    CharacterController player;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            isClicked = !isClicked;
+            isPlayerRotate = !isPlayerRotate;
+        }
+        Move();
+    }
+
+    void Move()
+    {
+        if(isClicked && !isPlayerRotate)
+        {
+            Vector3 di = Camera.main.transform.forward * SpeedMove;
+            player.SimpleMove(di);
+        }
+        
+        float h = Input.GetAxis("Horizontal");
+        if (isPlayerRotate) // player can now move
+        {
+            if (h>0 && isrotate)
+            {
+            
+                    transform.Rotate(Vector3.up, 45f );
+                    isrotate = false;
+                
+            }
+            else if (h < 0 && isrotate)  
+            {
+                    transform.Rotate(Vector3.up, -45f );
+                    isrotate = false;
+             
+            }  
+            else if (h==0 && !isClicked)
+            {
+                isrotate = true;
+            }
+        }
+    }
+   
+}
---
+++ Assets/MyScripts/myTeleport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class myTeleport : MonoBehaviour
+{
+    public Image Reticle;
+    bool isGazing;
+    float times;
+
+    [SerializeField]
+    float timer = 2f;
+
+    public List<GameObject> teleplace = new List<GameObject>();
+
+    private void Update()
+    {
+        if(isGazing)
+        {
+        times += Time.deltaTime;
+            Reticle.fillAmount = times / timer;
+        }
+        
+        if(Reticle.fillAmount == 1)
+        {
+            foreach( GameObject obj in teleplace )
+            {
+                transform.position = obj.transform.position;
+            }
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        isGazing = true;
+       
+        
+    }
+    public void OnPointerExit()
+    {
+        isGazing = false;
+        Reticle.fillAmount = 0;
+        times = 0f;
+    }    
+}
